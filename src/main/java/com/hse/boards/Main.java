4cab9dbd598511db435d751782be0fbf54dd4ba4
egiptipavel <kinqstone585@gmail.com>
2021-11-04 17:30:36 +0300

    Check style

--- src/main/java/com/hse/boards/Main.java
+++ src/main/java/com/hse/boards/Main.java
@@ -4,6 +4,8 @@ package com.hse.boards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class Main {
 
